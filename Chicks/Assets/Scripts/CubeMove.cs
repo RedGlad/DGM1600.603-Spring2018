f6301d50c8181f18d9820297c2a4099a3fcde28d
Unknown <gladengee@gmail.com>
2018-01-28 14:57:03 -0700

    Changed back to cube

--- Chicks/Assets/Scripts/CubeMove.cs
+++ Chicks/Assets/Scripts/CubeMove.cs
@@ -16,13 +16,13 @@ public class CubeMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	 void FixedUpdate () {
-		var y = Input.GetAxis("Jump")* Time.deltaTime * jumpHeight;
-		var x = Input.GetAxis("Horizontal")* Time.deltaTime * turnSpeed;
-		var z = Input.GetAxis("Vertical")* Time.deltaTime * moveSpeed;
+		var j = Input.GetAxis("Jump")* Time.deltaTime * jumpHeight;
+		var h = Input.GetAxis("Horizontal")* Time.deltaTime * turnSpeed;
+		var v = Input.GetAxis("Vertical")* Time.deltaTime * moveSpeed;
 		
-		rigid.AddForce(0,y,0);
+		rigid.AddForce(0,j,0);
 		// transform.Translate(0,j,0);
-		rigid.AddForce(x,0,0);
-		rigid.AddForce(0,0,z);
+		transform.Rotate(0,h,0);
+		transform.Translate(0,0,v);
     }
 }
