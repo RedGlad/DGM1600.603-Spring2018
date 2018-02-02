ba01adf5565f4efbb55903dd89239ded541072f2
Unknown <gladengee@gmail.com>
2018-02-01 19:53:05 -0700

    Did Gun Stuff

---
+++ Chicks/Assets/Scripts/Projectile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+	public int damage = 1;
+	public int time = 5;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(DestroyProjectile());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	IEnumerator DestroyProjectile(){
+		yield return new WaitForSeconds(time);
+		Destroy(gameObject);
+	}
+}
---
+++ Chicks/Assets/Scripts/Shoot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+	public Rigidbody projectile;
+	public Transform target;
+	public int shootSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Rigidbody clone;
+			clone = (Rigidbody)Instantiate(projectile, target.position, projectile.rotation);
+			clone.velocity = target.TransformDirection (Vector3.forward*shootSpeed*Time.deltaTime);
+		}
+	}
+}
