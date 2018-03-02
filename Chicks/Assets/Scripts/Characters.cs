fe4002776c4c11abd9fbf9203be5031b8e9037f3
Unknown <gladengee@gmail.com>
2018-03-01 17:05:39 -0700

    Added List Script
    Which probably won't get used.

---
+++ Chicks/Assets/Scripts/Characters.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Characters {
+
+    public string name;
+    public int sp;
+    public int jmtn;
+
+    public Characters(string newName, int newSp, int newJmtn) {
+        name = newName;
+        sp = newSp;
+        jmtn = newJmtn;
+    }
+
+}
