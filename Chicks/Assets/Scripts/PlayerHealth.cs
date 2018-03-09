7eb9306a4503c3fdd001853c0f3c7af065a88c65
Unknown <gladengee@gmail.com>
2018-03-08 19:53:24 -0700

    Woo he can DIE

---
+++ Chicks/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public const int maxHealth = 100;
+	public int currentHealth = maxHealth;
+	public Text hp;
+	public Text maxHp;
+	
+	void Update () {
+		hp.text = currentHealth.ToString();
+		maxHp.text = maxHealth.ToString();
+	}
+
+	public void TakeDamage(int amount) {
+		currentHealth -= amount;
+		if(currentHealth <= 0) {
+			currentHealth = 0;
+			print("Game Over");
+		}
+	}
+}
